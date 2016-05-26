8291381210f0727ee151b822d778e9c3b0dd8994
Nino Molina <leandro.e.molina@gmail.com>
2016-05-26 17:06:24 -0400

    added addComment method

--- app/modules/comments/commentController.js
+++ app/modules/comments/commentController.js
@@ -1,14 +1,31 @@
 'use strict';
+
+var comments = [{id: 1, comment: 'quiere aumento de sueldo', commentDate: '01/05/2016', creationDate: '21/05/2016', pm: 'yosu'},
+	        	{id: 2, comment: 'esta aburrido', commentDate: '02/05/2016', creationDate: '22/05/2016', pm: 'yosu'},
+	        	{id: 3, comment: 'quiere ver los partidos de la champios league', commentDate: '03/05/2016', creationDate: '23/05/2016', pm: 'yosu'}];
+
+var lastId = 3;
+
 function CommentController() {
 }
 
 CommentController.prototype = {
     constructor: CommentController,
     getComments: function ()  {
-        var data = [{comment: 'comment1', commentDate: '01/05/2016', creationDate: '25/05/2016', pm: 'yosu'},
-	        			{comment: 'comment1', commentDate: '01/05/2016', creationDate: '25/05/2016', pm: 'yosu'},
-	        			{comment: 'comment1', commentDate: '01/05/2016', creationDate: '25/05/2016', pm: 'yosu'}];
-	    return data;
+	    return comments;
+    },
+
+    addComment: function (comment, pm, commentDate) {
+    	var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+		var yyyy = today.getFullYear();
+
+		var creationDate = dd + '/' + mm + '/' + yyyy;
+		lastId++;
+		comments.push({id: lastId, comment: comment, commentDate: commentDate, creationDate: creationDate, pm: pm});
+
+		return lastId;
     }
 }
 
--- app/routes/comments.js
+++ app/routes/comments.js
@@ -8,6 +8,18 @@ module.exports = (function(router) {
 	    .get(function(req, res) {
 	        var data = controller.getComments();
 	        res.json(data);
-	});
+		})
+		.put(function(req, res) {
+			var body = req.body;
+
+			var comment = body.comment;
+			var pm = body.pm;
+			var commentDate = body.commentDate;
+
+			var id = controller.addComment(comment, pm, commentDate);
+
+			res.status(200).send({message: 'comment added!. Id ' + id});
+
+		});
 
 });
