e5a34d92d29db004a7f55b5cf0d3c1fd64ac94a3
Nino Molina <leandro.e.molina@gmail.com>
2016-05-26 14:06:20 -0400

    initial import

--- routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = (function(router) {
-    'use strict';
-    require('./app/routes/main')(router);
-    require('./app/routes/login')(router);
-    require('./app/routes/comments')(router);
-});
