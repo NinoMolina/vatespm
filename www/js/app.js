830e2078b3d917ca17af5802636d0cbd5c5a05f7
Jorge Carmona <jorgecarmona1986@gmail.com>
2016-05-30 14:31:45 -0400

    send comments from services

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,8 @@
-var app = angular.module('vatesApp',['login.loginService']);
+var app = angular.module('vatesApp',[]);
 
 
-app.controller('loginCtrl', ['$scope','LoginService', function($scope,LoginService){
+/*Controlador para login*/
+app.controller('loginCtrl', ['$scope','$http', function($scope,$http){
 
       $scope.invalido = false;
       $scope.cargando = false;
@@ -20,91 +21,25 @@ app.controller('loginCtrl', ['$scope','LoginService', function($scope,LoginServi
           }
           $scope.invalido=false;
           $scope.cargando=true;
-          LoginService.login($scope.datos).then(function(data){
-             if(data.logged == true){
-              window.location = 'templates/empleados.html';
-             }else{
-              $scope.invalido = true;
-              $scope.mensaje = "Usuario no existe";
-             }
+          var postObject = new Object();
+          postObject.user = $scope.datos.user;
+          postObject.password = $scope.datos.password;
+          $http.post('http://localhost:8080/api/login',postObject).success(function(data){
+               if(data.logged == true){
+                alert("usuario log ok");
+                window.location = 'templates/empleados.html';
+               }else{
+                $scope.invalido = true;
+                $scope.mensaje = "Usuario no existe";
+               } 
           });
       }
 }]);
 
-app.controller('listadoEmpleadosCtrl', function($scope) {
-  
-  $scope.data = {
-    showDelete: false
-  };
-  
-  $scope.edit = function(item) {
-    alert('Edit Item: ' + item.id);
-  };
-  $scope.share = function(item) {
-    alert('Share Item: ' + item.id);
-  };
-  
-  $scope.moveItem = function(item, fromIndex, toIndex) {
-    $scope.items.splice(fromIndex, 1);
-    $scope.items.splice(toIndex, 0, item);
-  };
-  
-  $scope.onItemDelete = function(item) {
-    $scope.items.splice($scope.items.indexOf(item), 1);
-  };
-  
-  $scope.items = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
-    { id: 11 },
-    { id: 12 },
-    { id: 13 },
-    { id: 14 },
-    { id: 15 },
-    { id: 16 },
-    { id: 17 },
-    { id: 18 },
-    { id: 19 },
-    { id: 20 },
-    { id: 21 },
-    { id: 22 },
-    { id: 23 },
-    { id: 24 },
-    { id: 25 },
-    { id: 26 },
-    { id: 27 },
-    { id: 28 },
-    { id: 29 },
-    { id: 30 },
-    { id: 31 },
-    { id: 32 },
-    { id: 33 },
-    { id: 34 },
-    { id: 35 },
-    { id: 36 },
-    { id: 37 },
-    { id: 38 },
-    { id: 39 },
-    { id: 40 },
-    { id: 41 },
-    { id: 42 },
-    { id: 43 },
-    { id: 44 },
-    { id: 45 },
-    { id: 46 },
-    { id: 47 },
-    { id: 48 },
-    { id: 49 },
-    { id: 50 }
-  ];
-  
-});
+/*Controlador para obtener a los empleados.*/
+app.controller('employeeCtrl', ['$scope','$http',function ($scope,$http) {
+      $http.get('http://localhost:8080/api/employees').success(function(data){
+       $scope.employee = data;
+      });
+}]);
+
