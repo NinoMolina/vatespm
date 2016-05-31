5e113d7f4a558dafc9461bd4497ad4065414806b
Jorge Carmona <jorgecarmona1986@gmail.com>
2016-05-31 16:31:00 -0400

    add system router for display pages

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
-var app = angular.module('vatesApp',[]);
+var app = angular.module('vatesApp',['ionic']);
 
 
 /*Controlador para login*/
-app.controller('loginCtrl', ['$scope','$http', function($scope,$http){
+app.controller('loginCtrl', ['$rootScope','$scope','$http','$state', function($rootScope,$scope,$http,$state){
 
       $scope.invalido = false;
       $scope.cargando = false;
@@ -27,7 +27,8 @@ app.controller('loginCtrl', ['$scope','$http', function($scope,$http){
           $http.post('http://localhost:8080/api/login',postObject).success(function(data){
                if(data.logged == true){
                 alert("usuario log ok");
-                window.location = 'templates/empleados.html';
+                $rootScope.username=postObject.user;
+                $state.go('listEmployees');
                }else{
                 $scope.invalido = true;
                 $scope.mensaje = "Usuario no existe";
@@ -37,9 +38,43 @@ app.controller('loginCtrl', ['$scope','$http', function($scope,$http){
 }]);
 
 /*Controlador para obtener a los empleados.*/
-app.controller('employeeCtrl', ['$scope','$http',function ($scope,$http) {
-      $http.get('http://localhost:8080/api/employees').success(function(data){
+app.controller('employeeCtrl', ['$rootScope','$scope','$http',function ($rootScope,$scope,$http) {
+      $http.get('http://localhost:8080/api/employees?pm='+$rootScope.username).success(function(data){
        $scope.employee = data;
       });
 }]);
 
+app.config(function ($stateProvider,$urlRouterProvider) {
+  $stateProvider.state('listEmployees',{
+    url:'/listEmployees',
+    templateUrl:'templates/listEmployees.html'
+  });
+  $urlRouterProvider.otherwise('/listEmployees');
+});
+
+app.config(function ($stateProvider,$urlRouterProvider) {
+  $stateProvider.state('login',{
+    url:'/login',
+    templateUrl:'templates/login.html'
+  });
+  $urlRouterProvider.otherwise('/login');
+});
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
